d8b53beb11ee6067d6d98a85a8832cabadc1b67e
Lihuwen <328445543@qq.com>
2017-11-01 15:24:14 +0800

    modify

--- js/scroll.js
+++ js/scroll.js
@@ -17,12 +17,12 @@ var TOPTmall = document.getElementById("TOP-Tmall");
 var TOPcombine = document.getElementById("TOP-combine");
 
 window.onscroll = function () {
-    if(document.body.scrollTop>=400){
+    if(document.documentElement.scrollTop>=400 || document.body.scrollTop>=400){
         TOPnav.style.display = "block";
     }else{
         TOPnav.style.display = "none";
     }
-    if(document.body.scrollTop>=700){
+    if(document.documentElement.scrollTop>=700 || document.body.scrollTop>=700){
         TOPnav.style.height = "50px";
         TOPTmall.style.visibility = "visible";
         TOPcombine.style.visibility = "visible";
@@ -32,55 +32,65 @@ window.onscroll = function () {
         TOPTmall.style.visibility = "hidden";
         TOPcombine.style.visibility = "hidden";
     }
-    if(document.body.scrollTop>=600){
+    if(document.documentElement.scrollTop>=600 || document.body.scrollTop>=600){
         LEFTnav.style.height = "323px";
         LEFTnav.style.width = "35px";
     }else{
         LEFTnav.style.height = "0";
         LEFTnav.style.width = "0";
     }
-    if(document.body.scrollTop>=1600 && document.body.scrollTop <2200){
+    if((document.documentElement.scrollTop>=1600 && document.documentElement.scrollTop <2200) ||
+        (document.body.scrollTop>=1600 && document.body.scrollTop <2200)){
         goELECTRONICS.style.backgroundColor ="dodgerblue";
     }else{
         goELECTRONICS.style.backgroundColor ="#636363";
     }
-    if(document.body.scrollTop>=2200 && document.body.scrollTop <2800){
+    if((document.documentElement.scrollTop>=2200 && document.documentElement.scrollTop <2800) ||
+        ((document.body.scrollTop>=2200 && document.body.scrollTop <2800))){
         goHOME.style.backgroundColor = "deeppink";
     }else{
         goHOME.style.backgroundColor ="#636363";
     }
-    if(document.body.scrollTop>=2800 && document.body.scrollTop <3400){
+    if((document.documentElement.scrollTop>=2800 && document.documentElement.scrollTop <3400) ||
+        (document.body.scrollTop>=2800 && document.body.scrollTop <3400)){
         goBEATY.style.backgroundColor = "hotpink";
     }else{
         goBEATY.style.backgroundColor ="#636363";
     }
-    if(document.body.scrollTop>=3400 && document.body.scrollTop <3900){
+    if((document.documentElement.scrollTop>=3400 && document.documentElement.scrollTop <3900) ||
+        (document.body.scrollTop>=3400 && document.body.scrollTop <3900)){
         goGROCERY.style.backgroundColor = "lawngreen";
     }else{
         goGROCERY.style.backgroundColor ="#636363";
     }
-    if(document.body.scrollTop>=3900 && document.body.scrollTop <4500){
+    if((document.documentElement.scrollTop>=3900 && document.documentElement.scrollTop <4500) ||
+        (document.body.scrollTop>=3900 && document.body.scrollTop <4500)){
         goOUTDOORS.style.backgroundColor = "lightseagreen";
     }else{
         goOUTDOORS.style.backgroundColor ="#636363";
     }
-    if(document.body.scrollTop>=4500 && document.body.scrollTop <5100){
+    if((document.documentElement.scrollTop>=4500 && document.documentElement.scrollTop <5100) ||
+        (document.body.scrollTop>=4500 && document.body.scrollTop <5100)){
         goKIDS.style.backgroundColor = "orange";
     }else{
         goKIDS.style.backgroundColor ="#636363";
     }
-    if(document.body.scrollTop>=5200){
+    if(document.documentElement.scrollTop>=5200 || document.body.scrollTop>=5200){
         goFAROURITE.style.backgroundColor = "deeppink";
     }else{
         goFAROURITE.style.backgroundColor ="#636363";
     }
+
+
+
+
 }
 
 var scroll = function (topvalue) {
     var i = 1;
     var UPscroll = function () {
         window.scrollBy(0, -50);
-        if (document.body.scrollTop <= topvalue) {
+        if (document.documentElement.scrollTop <= topvalue && document.body.scrollTop <= topvalue) {
 
             clearInterval(scrollmove);
             window.scrollTo(0,topvalue);
@@ -89,17 +99,17 @@ var scroll = function (topvalue) {
     }
     var DOWNscroll = function () {
         window.scrollBy(0, 50);
-        if (document.body.scrollTop >= topvalue) {
+        if (document.documentElement.scrollTop >= topvalue || document.body.scrollTop >= topvalue) {
 
             clearInterval(scrollmove);
             window.scrollTo(0,topvalue);
         }
 
     }
-    if (document.body.scrollTop > topvalue) {
+    if (document.documentElement.scrollTop> topvalue || document.body.scrollTop> topvalue) {
         var scrollmove = setInterval(UPscroll, 1);
     } else {
-        if(document.body.scrollTop < topvalue){
+        if(document.documentElement.scrollTop < topvalue || document.body.scrollTop < topvalue){
             var scrollmove = setInterval(DOWNscroll, 1);
         }
 
